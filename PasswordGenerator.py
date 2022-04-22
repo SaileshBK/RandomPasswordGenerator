63892774a8cbb8ca3b7285646534c18786fa262e
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-22 09:46:03 +0000

    comments

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -7,7 +7,7 @@ print("Welcome to Password generator!")
 #input the length of password 
 length = int(input('\nEnter the length of password: '))
 
-
+#our data
 #string.ascii_letters contains lower and uppercase alphabets
 # string.digits contains 0123456789
 # string.punctuation has all the symbols (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
