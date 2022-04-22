d071baeeeeb94b687a0de5df631fb49fc2f64500
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-22 09:43:12 +0000

    initial setup done

---
+++ PasswordGenerator.py
@@ -0,0 +1,19 @@
+#importing necessary modules
+import random
+import string
+
+
+print("Welcome to Password generator!")
+#input the length of password 
+length = int(input('\nEnter the length of password: '))
+
+
+#string.ascii_letters contains lower and uppercase alphabets
+# string.digits contains 0123456789
+# string.punctuation has all the symbols (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
+
+character = string.ascii_letters + string.digits + string.punctuation
+#use random charcaters to create the password.
+password = "".join(random.sample(character,length))
+#print the password
+print(password)
