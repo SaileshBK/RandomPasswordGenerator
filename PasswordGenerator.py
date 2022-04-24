0a9c85b09f8b588241d33e6a99574db7c3f44b5e
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-24 09:46:41 +0000

    new changes finished

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,11 +1,13 @@
 #importing necessary modules
 import random
 import string
+import os.path
 
 
 print("Welcome to Password generator!")
+passNumbers = int(input('\nEnter the number of password you want: '))
 #input the length of password 
-length = int(input('\nEnter the length of password: '))
+length = int(input('\nEnter the length of each password: '))
 
 #our data
 #string.ascii_letters contains lower and uppercase alphabets
@@ -14,6 +16,42 @@ length = int(input('\nEnter the length of password: '))
 
 character = string.ascii_letters + string.digits + string.punctuation
 #use random charcaters to create the password.
-password = "".join(random.sample(character,length))
-#print the password
-print(password)
+
+
+
+file_exists = os.path.exists('passwords.txt')
+
+try:
+  if file_exists==True:
+    os.remove("passwords.txt")
+    newFile = open('passwords.txt', 'x')
+    newFile.write("\nList of " +str(passNumbers)+" passwords with "+str(length)+ " length :\n")
+    # newFile.write(password)
+    for i in range(1,passNumbers+1):
+      newFile.write(str(i)+". ")
+      newFile.write("".join(random.sample(character,length))+"\n")
+    
+    newFile = open('passwords.txt', 'r')
+    data = newFile.read()
+    print(data)
+    newFile.close()
+
+
+    
+    # f = open("passwords.txt")
+    # print(f.read())
+    # f.close()
+  else:
+    newFile = open('passwords.txt', 'x')
+    newFile.write("\nList of " +str(passNumbers)+" passwords with "+str(length)+ " length :\n")
+    # newFile.write(password)
+    for i in range(1,passNumbers+1):
+      newFile.write(str(i)+". ")
+      newFile.write("".join(random.sample(character,length))+"\n")
+    
+    newFile = open('passwords.txt', 'r')
+    data = newFile.read()
+    print(data)
+    newFile.close()
+except FileNotFoundError:
+    print("The 'docs' directory does not exist")
