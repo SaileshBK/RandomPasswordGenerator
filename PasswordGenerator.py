bd0556270563039263440db586ad120d049023df
Sailesh B K <101400043+SaileshBK@users.noreply.github.com>
2022-04-25 00:19:40 +0000

    finished

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,55 +3,51 @@ import random
 import string
 import os.path
 
+def main():
+  
+  print("Welcome to Password generator!")
+  passNumbers = int(input('\nEnter the number of password you want: '))
+  #input the length of password 
+  length = int(input('\nEnter the length of each password: '))
 
-print("Welcome to Password generator!")
-passNumbers = int(input('\nEnter the number of password you want: '))
-#input the length of password 
-length = int(input('\nEnter the length of each password: '))
-
-#our data
-#string.ascii_letters contains lower and uppercase alphabets
-# string.digits contains 0123456789
-# string.punctuation has all the symbols (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
-
-character = string.ascii_letters + string.digits + string.punctuation
-#use random charcaters to create the password.
-
-
-
-file_exists = os.path.exists('passwords.txt')
-
-try:
-  if file_exists==True:
-    os.remove("passwords.txt")
-    newFile = open('passwords.txt', 'x')
-    newFile.write("\nList of " +str(passNumbers)+" passwords with "+str(length)+ " length :\n")
-    # newFile.write(password)
-    for i in range(1,passNumbers+1):
-      newFile.write(str(i)+". ")
-      newFile.write("".join(random.sample(character,length))+"\n")
-    
-    newFile = open('passwords.txt', 'r')
-    data = newFile.read()
-    print(data)
-    newFile.close()
-
-
-    
-    # f = open("passwords.txt")
-    # print(f.read())
-    # f.close()
-  else:
-    newFile = open('passwords.txt', 'x')
-    newFile.write("\nList of " +str(passNumbers)+" passwords with "+str(length)+ " length :\n")
-    # newFile.write(password)
-    for i in range(1,passNumbers+1):
-      newFile.write(str(i)+". ")
-      newFile.write("".join(random.sample(character,length))+"\n")
-    
-    newFile = open('passwords.txt', 'r')
-    data = newFile.read()
-    print(data)
-    newFile.close()
-except FileNotFoundError:
+  #our data
+  #string.ascii_letters contains lower and uppercase alphabets
+  # string.digits contains 0123456789
+  # string.punctuation has all the symbols (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
+  #use random charcaters to create the password.
+  character = string.ascii_letters + string.digits + string.punctuation
+  
+  file_exists = os.path.exists('passwords.txt')
+  try:
+    if file_exists==True:
+      #This removes the text file if it exists to create a new one.
+      os.remove("passwords.txt")
+      print(passGenerator(passNumbers,length,character))
+  
+    else:
+       print(passGenerator(passNumbers,length,character))
+  except FileNotFoundError:
     print("The 'docs' directory does not exist")
+
+
+#password generator function
+def passGenerator(passNumbers,length,character):
+  #creating a new file.
+  newFile = open('passwords.txt', 'x')
+  newFile.write("\nList of " +str(passNumbers)+" passwords with "+str(length)+ " length :\n")
+  # writing passwords in new text file.
+  for i in range(1,passNumbers+1):
+    newFile.write(str(i)+". ")
+    newFile.write("".join(random.sample(character,length))+"\n")
+
+  #Making the file readable
+  newFile = open('passwords.txt', 'r')
+  data = newFile.read()
+  newFile.close()
+
+  return data
+
+
+#main
+if __name__ =="__main__":
+  main()
